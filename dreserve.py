36ef14af2af2beaec86f132e64cb5c4d8ecd1c48
Jomari Peterson <jomari.peterson@hajjmedia.com>
2017-11-15 17:03:51 -0800

    Customized Python Matrix Bot
    Customized for the Digital Reserve

--- dreserve.py
+++ dreserve.py
@@ -2,7 +2,7 @@
 A test bot using the Python Matrix Bot API
 
 Test it out by adding it to a group chat and doing one of the following:
-1. Say "Hi"
+1. Respond to greetings aloha, hi and hello
 2. Say !echo this is a test!
 3. Say !d6 to get a random size-sided die roll result
 """
@@ -14,14 +14,22 @@ from matrix_bot_api.mregex_handler import MRegexHandler
 from matrix_bot_api.mcommand_handler import MCommandHandler
 
 # Global variables
-USERNAME = ""  # Bot's username
-PASSWORD = ""  # Bot's password
-SERVER = ""  # Matrix server URL
+USERNAME = "dreserve"  # Bot's username
+PASSWORD = "Sessriotcomemobile1"  # Bot's password
+SERVER = "http://matrix.org"  # Matrix server URL
+
+adios=["aloha","hi","hello"]				#create a list of words
 
 
 def hi_callback(room, event):
     # Somebody said hi, let's say Hi back
+    args = event['content']['body'].split()      #find the words in the message body
+    try:
+        print(adios.index(' '.join(set(adios)&set(args))))       #find the index value for the message body words which match the list
+    except ValueError:
+        print("No Match")
     room.send_text("Hi, " + event['sender'])
+	
 
 
 def echo_callback(room, event):
@@ -59,11 +67,13 @@ def dieroll_callback(room, event):
 def main():
     # Create an instance of the MatrixBotAPI
     bot = MatrixBotAPI(USERNAME, PASSWORD, SERVER)
-
-    # Add a regex handler waiting for the word Hi
-    hi_handler = MRegexHandler("Hi", hi_callback)
-    bot.add_handler(hi_handler)
-
+	
+       # Add a regex handler waiting for the words from a list defined in global up top
+    for index,item in enumerate(adios):								#loop through the word selection for index and item - may need to be 
+        							
+        hi_handler = MRegexHandler(item, hi_callback)
+        bot.add_handler(hi_handler)
+		  
     # Add a regex handler waiting for the echo command
     echo_handler = MCommandHandler("echo", echo_callback)
     bot.add_handler(echo_handler)
@@ -78,6 +88,11 @@ def main():
     # Infinitely read stdin to stall main thread while the bot runs in other threads
     while True:
         input()
+        msg = in_common.get_input()   #doesnt work currently
+        if msg == "/quit":
+            break
+        else:
+            room.send_text(msg)
 
 
 if __name__ == "__main__":
